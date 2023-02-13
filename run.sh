54e5f17be904396f8c9035ee28651ce392b0fd1a
mezziamani2809 <55669353+mezziamani2809@users.noreply.github.com>
2023-02-13 19:59:23 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@ sed -i 's/20015/'$CHAT'/g' $DESTINATION/docker-compose.yml
 # run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 
-echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: Aker@1234 | Live chat port: '$CHAT
+echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: dar_zaghouan@1234 | Live chat port: '$CHAT
